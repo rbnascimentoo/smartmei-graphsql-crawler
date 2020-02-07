649558e1d207e873a972e2986d841d73227b374b
rbnascimentoo <rnascimentoo@live.com>
2020-02-07 00:24:20 -0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var express_graphql = require('express-graphql');
+const request = require('request');
+const cheerio = require('cheerio');
+var { buildSchema } = require('graphql');
+
+// Schema
+var schema = buildSchema(`
+    type Query {
+        startCrawler(nameHost: String!): Retorno
+    }
+
+    type Retorno {
+        date: String,
+        descricao: String,
+        valorReal: Float,
+        valorUSD: Float,
+        valorEUR: Float
+    }
+`);
+
+// Maps username to content
+var retorno = {};
+
+// Mapeamento
+var root = {
+    call: ({nameHost}) => {
+
+        if(nameHost.indexOf('www.smartmei.com.br') !== -1) {
+            return retorno;
+        }
+
+        request(nameHost, function (err, res, body) {
+            if(err) {
+                console.log(err, "error occured while hitting URL");
+            } else {
+                console.log(body);
+            }
+        });
+
+        //Teste
+        retorno.date = '08/02/2020';
+
+        return retorno;
+    }
+};
+
+var app = express();
+app.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(3000, () => console.log('Express GraphQL Server Now Running On localhost:3000/graphql'));
